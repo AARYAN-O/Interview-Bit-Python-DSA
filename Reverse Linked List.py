8eaad32befa0e329193e377f76b0831446263733
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-09-12 14:10:10 +0530

    Update Reverse Linked List.py

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -8,17 +8,17 @@ class Solution:
 	# @param A : head node of linked list
 	# @return the head node in the linked list
 	def reverseList(self, A):
-        prev=None
-        current=A
+	        prev=None
+	        current=A
 	# currrent.next contains the address of the next node.
 	# prev is a Node and current.next is a pointer to the next node.
 	# Then, how can two things having different datatypes be on the either sides of equals to operator.
 	# This is so because they are both part of same Nodes.
 	
-        while current:
-            next_node=current.next
-            current.next=prev
-            prev=current
-            current=next_node
-        
-        return prev
+	        while current:
+	            next_node=current.next
+	            current.next=prev
+	            prev=current
+	            current=next_node
+	        
+	        return prev
